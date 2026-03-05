4f5968c85ded1c45bc1b59642f843437eaa67a53
diyasilawat10 <diyasilawat10@gmail.com>
2026-03-05 15:46:54 +0530

    feat: added NewProducts component and product images

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Landing page/Navbar/Navbar";
 import Hero from "./components/Landing page/HeroSection/Hero";
 import Categories from "./components/Landing page/Categories/Categories";
 import ProductSection from "./components/Landing page/ProductSection/ProductSection";
+import NewProducts from "./components/Landing page/NewProducts/NewProducts";
 import PromoSection from "./components/Landing page/PromoSection/PromoSection";
 import Footer from "./components/Landing page/Footer/Footer";
 
@@ -12,6 +13,7 @@ function App() {
       <Hero />
       <Categories />
       <ProductSection />
+      <NewProducts />
       <PromoSection />
       <Footer />
     </>
