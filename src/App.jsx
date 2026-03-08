9e225847f0f3bd07d7bac139ec5bb393d579012d
diyasilawat10 <diyasilawat10@gmail.com>
2026-03-08 23:29:28 +0530

    feat: add services section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Landing page/HeroSection/Hero";
 import Categories from "./components/Landing page/Categories/Categories";
 import ProductSection from "./components/Landing page/ProductSection/ProductSection";
 import PromoSection from "./components/Landing page/PromoSection/PromoSection";
+import ServiceBar from "./components/Landing page/ServiceBar/ServiceBar";
 import Footer from "./components/Landing page/Footer/Footer";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Categories />
       <ProductSection />
       <PromoSection />
+      <ServiceBar />
       <Footer />
     </>
   );
