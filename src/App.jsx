3e6f9a80a35e7ec6f77dc7640796f12be3aa263a
diyasilawat10 <diyasilawat10@gmail.com>
2026-03-10 16:02:51 +0530

    Add BestSellers section and fix UI spacing

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Categories from "./components/Landing page/Categories/Categories";
 import ProductSection from "./components/Landing page/ProductSection/ProductSection";
 import NewProducts from "./components/Landing page/NewProducts/NewProducts";
 import PromoSection from "./components/Landing page/PromoSection/PromoSection";
+import BestSellers from "./components/Landing page/BestSellers/BestSellers";
 import ServiceBar from "./components/Landing page/ServiceBar/ServiceBar";
 import Footer from "./components/Landing page/Footer/Footer";
 
@@ -16,6 +17,7 @@ function App() {
       <ProductSection />
       <NewProducts />
       <PromoSection />
+      <BestSellers />
       <ServiceBar />
       <Footer />
     </>
