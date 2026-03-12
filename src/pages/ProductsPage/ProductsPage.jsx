85ef27eb1fb957c723accac8e7d326c5fa2fdab5
diyasilawat10 <diyasilawat10@gmail.com>
2026-03-12 12:20:05 +0530

    feat: add product categories component

--- src/pages/ProductsPage/ProductsPage.jsx
+++ src/pages/ProductsPage/ProductsPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './ProductsPage.css';
 import Breadcrumb from '../../components/Products page/Breadcrumb/Breadcrumb';
+import ProductCategories from '../../components/Products page/ProductCategories/ProductCategories';
 
 const ProductsPage = () => {
   return (
     <div className="products-page">
       <div className="products-page-container">
         <Breadcrumb />
+        <ProductCategories />
         {/* Further components will be added here */}
       </div>
     </div>
